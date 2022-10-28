31c25aa431837196812235764f200fb455d83244
JPGenaro <71389486+JPGenaro@users.noreply.github.com>
2022-10-28 10:05:36 -0300

    Agregando puntos y comas
    Cierta persona se dejo el ";" en el manual de java al parecer

--- database.sql
+++ database.sql
@@ -5,7 +5,7 @@ create table Documento(
     id int PRIMARY KEY auto_increment,
     tipo varchar(30),
     numero int
-)
+);
 
 create table Cliente(
     id int PRIMARY KEY auto_increment,
@@ -17,13 +17,13 @@ create table Cliente(
     cuit int,
     id_Documento int,
     constraint id_Documento foreign key (id_Documento) references Documento(id)
-)
+);
 
 create table FormaPago(
     id int PRIMARY KEY auto_increment,
     nombre varchar(40),
     descripcion varchar(100)
-)
+);
 
 create table Venta(
     id int PRIMARY KEY auto_increment,
@@ -33,13 +33,13 @@ create table Venta(
     id_Cliente int,
     constraint id_FormaPago foreign key (id_FormaPago) references FormaPago(id),
     constraint id_Cliente foreign key (id_Cliente) references Cliente(id)
-)
+);
 
 create table TipoProducto(
     id int PRIMARY KEY auto_increment,
     nombre varchar(50),
     descripcion varchar(50)
-)
+);
 
 create table Producto(
     id int PRIMARY KEY auto_increment,
@@ -48,7 +48,7 @@ create table Producto(
     stock int,
     id_TipoProducto int,
     constraint id_TipoProducto foreign key (id_TipoProducto) references TipoProducto(id)
-)
+);
 
 create table DetalleProductoVenta(
     id int PRIMARY KEY auto_increment,
@@ -58,4 +58,4 @@ create table DetalleProductoVenta(
     id_Venta int,
     constraint id_Producto foreign key (id_Producto) references Producto(id),
     constraint id_Venta foreign key (id_Venta) references Venta(id)
-)
+);
