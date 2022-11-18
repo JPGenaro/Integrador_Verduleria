0cc783254d1567c57ef8e9db6186c16c075119e3
Lichi <lichimarquez04@gmail.com>
2022-11-18 08:50:18 -0300

    Admin Site Starting

--- Proyecto/Verduleria/admin.py
+++ Proyecto/Verduleria/admin.py
@@ -4,9 +4,30 @@ from .models import Cliente, Venta, Tipoproducto, Documento, Formapago, Producto
 # Register your models here.
 
 admin.site.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ("telefono","mail")
+    list_filter = ("apellido")
 admin.site.register(Venta)
+class VentaAdmin(admin.ModelAdmin):
+    list_display = ("id", "fecha", "id_formapago.nombre")
+    list_filter = ("id")
 admin.site.register(Tipoproducto)
+class TipoProductoAdmin(admin.ModelAdmin):
+    list_display = ("descripcion")
+    list_filter = ("nombre")
 admin.site.register(Documento)
+class DocumentoAdmin(admin.ModelAdmin):
+    list_display = ("tipo")
+    list_filter = ("numero")
 admin.site.register(Formapago)
+class FormapagoAdmin(admin.ModelAdmin):
+    list_display = ("descripcion")
+    list_filter = ("nombre")
 admin.site.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ("stock","id_tipoproducto.nombre")
+    list_filter = ("apellido")
 admin.site.register(Detalleproductoventa)
+class DetalleproductoventaAdmin(admin.ModelAdmin):
+    list_display = ("id", "precioproducto", "cantidadproducto")
+    list_filter = ("id")
