3db595c9d64e5222764778709d4f326a728fab36
JPGenaro <jpgenaro06@gmail.com>
2022-11-24 10:34:26 -0300

    Solucion de pequeños errores

--- Proyecto/Verduleria/views.py
+++ Proyecto/Verduleria/views.py
@@ -4,4 +4,4 @@ from .models import *
 # Create your views here.
 def home(request):
     dato = Producto.objects.all().filter(id_tipoproducto = 1)[2]
-    return render(request, '../templates/Verduleria/main.html',{'producto':dato})
+    return render(request, '../templates/Verduleria/index.html')#{'producto':dato})
