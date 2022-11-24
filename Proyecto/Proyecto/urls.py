bc27f56363fcbbd812e9c3f2981a5e72ab2f8f9b
JPGenaro <jpgenaro06@gmail.com>
2022-11-24 08:48:13 -0300

    Comenzando con las Urls

--- Proyecto/Proyecto/urls.py
+++ Proyecto/Proyecto/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from Verduleria.views import bienvenida
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', bienvenida),
 ]
